22f8235ed6cc4cc3d3b65088130e102f66e4595f
Dave Longley <dlongley@digitalbazaar.com>
2019-09-04 17:44:04 -0400

    Add BrQramScanner component.

--- BrQramScanner.vue
+++ BrQramScanner.vue
@@ -8,13 +8,60 @@
   */
 'use strict';
 
+import {QramScanner} from './QramScanner.js';
+
 export default {
   name: 'BrQramScanner',
   props: {
+    source: {
+      type: Object,
+      default: null,
+      required: true
+    }
   },
   data() {
     return {
+      scanning: false,
+      scanner: null
     };
+  },
+  methods: {
+    async scan() {
+      if(!this.scanner) {
+        this.scanner = new QramScanner();
+      }
+
+      if(this.scanning) {
+        this.scanning = false;
+        this.scanner.cancel();
+        return;
+      }
+
+      this.scanning = true;
+      const {source: {$el: source}} = this;
+      const start = Date.now();
+      const result = await this.scanner.scan({
+        source,
+        progress(event) {
+          console.log('progress', event);
+        }
+      });
+      console.log('scan complete', result);
+      const {
+        blocks,
+        receivedPackets,
+        receivedBlocks,
+        totalBlocks
+      } = result;
+      console.log(`Decoded ${receivedBlocks}/${totalBlocks} blocks`);
+      const time = ((Date.now() - start) / 1000).toFixed(3);
+      const {data} = result;
+      // console.log('decoded data', data);
+      const size = (data.length / 1024).toFixed(3);
+      const msg = `Decoded ${size} KiB in time ${time} seconds`;
+      console.log(msg);
+      this.scanning = false;
+    }
   }
 };
 </script>
--- index.js
+++ index.js
@@ -4,3 +4,6 @@
 'use strict';
 
 export {default as BrQramPresenter} from './BrQramPresenter.vue';
+export {default as BrQramScanner} from './BrQramScanner.vue';
+// TODO: determine if this should be exposed or moved to another lib
+//export {default as QramScanner} from './QramScanner.js';
